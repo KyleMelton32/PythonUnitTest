a18b7c6c7b978a97869e15daaae0bf6c480d0521
Kyle Melton <KyleMelton32@gmail.com>
2020-07-22 02:25:31 -0600

    Adding another test example

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,16 +1,17 @@
 import cmath
 
-
 def quadratic_equation(a, b, c):
     """
         This method solves the quadratic given a, c, b from the form ax^2+bx+c=0.
         If the quadratic has multiple solutions, this will return them in a list.
         Since quadratics can have complex solutions, use cmath to compute the square root.
-        If a divide by zero would occur, return None.
+        If a divide by zero would occur, raise an exception.
         @:param a integer
         @:param b integer
         @:param c integer
     """
+    # To make things easier, I've included the math already.
     positive_solution = (-b + cmath.sqrt(b ** 2 - (4 * a * c)))/(2 * a)
     negative_solution = (-b - cmath.sqrt(b ** 2 - (4 * a * c)))/(2 * a)
+
     return positive_solution, negative_solution
--- quadratic_equation_test.py
+++ quadratic_equation_test.py
@@ -1,21 +1,30 @@
 import unittest
 import quadratic_equation
 
+
 class QuadraticEquationTest(unittest.TestCase):
     def test_positive(self):
+        """ This tests when the discriminate is positive. (b^2 - 4 * a * c) > 0 """
         positive_solution, negative_solution = \
             quadratic_equation.quadratic_equation(5, 6, 1)
         self.assertEqual(positive_solution.real, -0.2)
         self.assertEqual(negative_solution.real, -1)
 
+    @unittest.skip("Test not developed")
     def test_zero(self):
-        self.assertEqual(True, True)
+        """ This tests when the discriminate is zero. (b^2 - 4 * a * c) = 0 """
+        pass
 
+    @unittest.skip("Test not developed")
     def test_negative(self):
-        self.assertEqual(True, True)
+        """ This tests when the discriminate is negative. (b^2 - 4 * a * c) < 0 """
+        pass
 
     def test_divide_by_zero(self):
-        self.assertEqual(True, True)
+        """ This tests when the a divide by zero would occur (a=0) """
+        with self.assertRaises(ZeroDivisionError):
+            positive_solution, negative_solution = \
+                quadratic_equation.quadratic_equation(0, 6, 1)
 
 
 if __name__ == '__main__':
